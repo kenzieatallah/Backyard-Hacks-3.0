bd17174d395267321218bc0482425d421466018f
Kenzie Atallah <>
2022-07-31 07:33:57 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+// var navButton = document.getElementById("navButton");
+// var ul = document.getElementById("ul");
+
+// navButton.addEventListener("click", animateNav);
+
+// function animateNav(){
+//   if (ul.display == "none") {
+//     ul.display = "block";
+//   }
+//   else {
+//     ul.display = "none";
+//   }
+// }
